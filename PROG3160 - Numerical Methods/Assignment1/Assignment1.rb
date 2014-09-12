731642ef99a541fd63680043d3c036bfccb2cd2e
Verdi R-D <verdi@azend.org>
2014-09-12 10:47:42 -0400

    Finished assignment program

--- PROG3160 - Numerical Methods/Assignment1/Assignment1.rb
+++ PROG3160 - Numerical Methods/Assignment1/Assignment1.rb
@@ -1,4 +1,9 @@
 
+# PROG3160 - Numerical Methods 3000
+# Assignment #1
+# Verdi R-D
+
+# Get the x value from the user
 print 'What is X? '
 x = gets.chomp!
 x = x.to_f
@@ -7,6 +12,7 @@ unless x > 0
 	abort('ERROR: Did not enter a value greater than 0')
 end
 
+# Get the k value from the user
 print 'What is K? '
 k = gets.chomp!
 k = k.to_i
@@ -18,10 +24,21 @@ end
 i = 1
 sum = 0
 
+# Calculate the fraction
+fraction = 2.0 / ( 2.0 - x )
+
+puts '' 
+puts "Fraction = #{ sprintf '%.6f', fraction }"
+puts ''
+puts 'Approximations:'
+
+# Calculate our approximations
 while i <= k do
 	sum += ((x * 0.5) ** (i - 1))
+	absolute_error = ( fraction - sum ).abs
+	relative_error = ( absolute_error / fraction ) * 100
 
-	puts "x = #{ x }, k = #{ k }, i = #{ i }, sum = #{ sum }"
+	puts "x = #{ x }, k = #{ k }, i = #{ i }, sum = #{ sprintf "%.6f", sum } error = #{ sprintf "%.2e", relative_error }"
 
 	i += 1
 end
