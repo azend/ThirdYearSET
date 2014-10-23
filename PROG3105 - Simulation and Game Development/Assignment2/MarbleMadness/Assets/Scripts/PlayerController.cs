0f7e6e197830b5181e3583c609f260eef98ee472
Verdi R-D <verdi@azend.org>
2014-10-23 12:20:34 -0400

    Completed unity roll-a-ball tutorial

---
+++ PROG3105 - Simulation and Game Development/Assignment2/MarbleMadness/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GUIText countText;
+	public GUIText winText;
+	public float speed;
+	private int count;
+
+	void Start ()
+	{
+		count = 0;
+
+
+		UpdateCount ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Pickup")
+		{
+			other.gameObject.SetActive(false);
+			count ++;
+
+			UpdateCount();
+		}
+	}
+
+	void UpdateCount ()
+	{
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 8) {
+				winText.text = "You've won!";
+		} else {
+				winText.text = string.Empty;
+		}
+	}
+}
